993b304320493d7de42a06884d1245b492a4c52d
abirbu <ariful.islam2874@gmail.com>
2023-01-22 12:40:44 +0600

    first commit

---
+++ Resources/JS/script.js
@@ -0,0 +1,14 @@
+//MixitUp
+
+var mixer = mixitup('.grid-portfolio-container');
+
+
+// Function for opening nav
+
+function OpenMenu(){
+    document.getElementById('ham-nav-menu').style.display= 'block';
+}
+
+function CloseMenu(){
+    document.getElementById('ham-nav-menu').style.display ='none';
+}
